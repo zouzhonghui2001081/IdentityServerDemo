d803fd1d3722daf02fbc22c94c7f5121f0b2b30c
zouzhonghui2001081 <zhonghui.zou@perkinelmer.com>
2022-04-25 23:05:44 +0800

    add entity framework support for identity server 4

--- IdentityServerDemo/Demo.IdentityServer/Program.cs
+++ IdentityServerDemo/Demo.IdentityServer/Program.cs
@@ -1,13 +1,36 @@
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMvc(opt => opt.EnableEndpointRouting = false); 
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", false)
+    .Build();
+
+var conString = config.GetConnectionString("identityserver4db");
+var migrationAssembly = typeof(Program).Assembly.GetName().Name;
+
 builder.Services.AddIdentityServer()
     .AddDeveloperSigningCredential()
-    .AddInMemoryIdentityResources(Config.GetIdentityResources())
-    .AddInMemoryApiResources(Config.GetApiResources())
-    .AddInMemoryApiScopes(Config.GetApiScopes())
-    .AddInMemoryClients(Config.GetClients())
-    .AddTestUsers(Config.GetTestUsers());
+    .AddTestUsers(Config.GetTestUsers())
+    .AddConfigurationStore(options =>
+    {
+        // stores clients and resources
+        options.ConfigureDbContext = context => context.UseNpgsql(conString, pgsql => pgsql.MigrationsAssembly(migrationAssembly));
+    })
+    .AddOperationalStore(options =>
+    {
+        // stores tokens, consents, codes
+        options.ConfigureDbContext = context => context.UseNpgsql(conString, pgsql => pgsql.MigrationsAssembly(migrationAssembly));
+    });
+//.AddInMemoryIdentityResources(Config.GetIdentityResources())
+//.AddInMemoryApiResources(Config.GetApiResources())
+//.AddInMemoryApiScopes(Config.GetApiScopes())
+//.AddInMemoryClients(Config.GetClients())
+//.AddTestUsers(Config.GetTestUsers());
 
 // fixing The cookie 'idsrv' has set 'SameSite=None' and must also set 'Secure'. issue
 builder.Services.Configure<CookiePolicyOptions>(options =>
@@ -17,6 +40,8 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
 
 var app = builder.Build();
 
+InitializeIdentityServer4DB(app);
+
 // fixing The cookie 'idsrv' has set 'SameSite=None' and must also set 'Secure'. issue
 app.UseCookiePolicy();
 app.UseIdentityServer();
@@ -24,3 +49,45 @@ app.UseStaticFiles();
 app.UseMvcWithDefaultRoute();
 
 app.Run();
+
+
+
+void InitializeIdentityServer4DB(WebApplication app)
+{
+    using(var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+    {
+        var persistedDbContext = serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>();
+        persistedDbContext.Database.Migrate();
+
+        var configDbContext = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+        configDbContext.Database.Migrate();
+
+        // Seed the data
+        if(!configDbContext.Clients.Any())
+        {
+            foreach(var client in Config.GetClients())
+            {
+                configDbContext.Clients.Add(client.ToEntity());
+            }
+
+            configDbContext.SaveChanges();
+        }
+        if(!configDbContext.IdentityResources.Any())
+        {
+            foreach (var resource in Config.GetIdentityResources())
+            {
+                configDbContext.IdentityResources.Add(resource.ToEntity());
+            }
+            configDbContext.SaveChanges();
+        }
+        if(!configDbContext.ApiResources.Any())
+        {
+            foreach(var apiResource in Config.GetApiResources())
+            {
+                configDbContext.ApiResources.Add(apiResource.ToEntity());
+            }
+            configDbContext.SaveChanges();
+        }
+
+    }
+}
